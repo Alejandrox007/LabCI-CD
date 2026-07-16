3efbbe14b21a1ad28758b4332cf93db507b38372
Alejandrox007 <alejomontufar393@gmail.com>
2026-07-16 11:49:37 -0500

    Preparar API para despliegue en Vercel

--- index.js
+++ index.js
@@ -2,8 +2,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Integracion continua');
+  res.status(200).json({ message: 'CI/CD funcionando correctamente' , 
+    servicio:'API Express desplegada en vercel'});
+});
+
+app.get('/saludo/:nombre', (req, res) => {
+  const { nombre } = req.params;
+  res.status(200).json({ message: `¡Hola, ${nombre}!`,
+  estado:'API disponible' });
+});
+
+app.get('/suma', (req, res) => {
+  const num1  = Number(req.query.num1);
+  const num2 = Number(req.query.num2);
+  if(Number.isNaN(num1) || Number.isNaN(num2)){
+    return res.status(400).json({ error: 'Los parámetros num1 y num2 deben ser números válidos' });
+  }
+  
+  res.status(200).json({num1, num2, resultado: num1 + num2 });
 });
 
 app.listen(port, () => {
